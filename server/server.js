0c1c7a55ed8315976f9e9059c3dc30f3e1aacee7
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-27 15:06:24 +0700

    ลองสร้าง Route
    สร้าง path /product และให้แสดงผลข้อความ Hello endpoint

--- server/server.js
+++ server/server.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const app = express();
 
+// Route 1
+app.get('/product',(req,res)=>(
+    res.send('Hello EndPoint')
+))
 app.listen(5000,()=> console.log('Server is running on port 5000'))
